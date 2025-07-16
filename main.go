26ed159b080c44a7ca031957dd1859322aa79adc
nuggetplum <notaditya2k4@gmail.com>
2025-07-16 20:19:58 +0000

    added config file

--- main.go
+++ main.go
@@ -8,16 +8,21 @@ import (
 
 	"github.com/nuggetplum/simplebank/api"
 	db "github.com/nuggetplum/simplebank/db/sqlc"
+	"github.com/nuggetplum/simplebank/util"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +30,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
